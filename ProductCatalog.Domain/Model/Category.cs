c2dd455ac6956a09db42381b7d517d67bfb77db1
akuklinov <akuklinov@bmfn.com>
2023-11-18 20:23:56 +0200

    Implementing domain models.

---
+++ ProductCatalog.Domain/Model/Category.cs
@@ -0,0 +1,16 @@
+﻿using ProductCatalog.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductCatalog.Domain.Model
+{
+    public class Category: IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
---
+++ ProductCatalog.Domain/Model/Product.cs
@@ -0,0 +1,19 @@
+﻿using ProductCatalog.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductCatalog.Domain.Model
+{
+    public class Product: IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int AvailableCount { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
